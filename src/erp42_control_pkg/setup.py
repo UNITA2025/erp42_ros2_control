ca15f27592fd647f43fd3e93c2c4860a907f553f
Hyeongjin_Kim <ky942400@gmail.com>
2025-08-02 03:32:03 +0900

    struct update

---
+++ src/erp42_control_pkg/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'erp42_control_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='moonshot',
+    maintainer_email='ky942400@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ByteHandler_node = erp42_control_pkg.ByteHandler:main',
+            'ErpSerialHandler_node = erp42_control_pkg.ErpSerialHandler:main',
+        ],
+    },
+)
